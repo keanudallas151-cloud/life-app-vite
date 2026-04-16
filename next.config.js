000440327b51ed79dd5296b3f7efa8f159e2cfba
Life. <keanudallas151@gmail.com>
2026-04-16 21:00:47 +0930

    Update next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-=======
-  distDir: 'dist',
->>>>>>> 03fa3a525356c4a391918ebe5bd072904c82eed1
   images: {
     unoptimized: true
   }
